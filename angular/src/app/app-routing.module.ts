f2f09c3106117973da82b6fe4c75e623acaebd78
hessamasna <hessamasna@yahoo.com>
2021-08-17 11:52:00 +0430

    work on login & signUp & landing and routing

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AllsongsComponent } from './dashboard/allsongs/allsongs.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AllsongsComponent} from './dashboard/allsongs/allsongs.component';
+import {LandingComponent} from "./landing/landing.component";
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'landing', component: LandingComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
   {
     path: 'dashboard',
     component: DashboardComponent,
     children: [
-      { path: '', redirectTo: 'allsongs', pathMatch: 'full' },
-      {
-        path: 'allsongs',
-        component: AllsongsComponent,
-      },
+      {path: '', redirectTo: 'allsongs', pathMatch: 'full'},
+      {path: 'allsongs', component: AllsongsComponent},
     ],
   },
 ];
@@ -22,4 +25,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
