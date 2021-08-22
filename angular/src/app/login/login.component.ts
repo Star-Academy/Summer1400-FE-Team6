f41b80fbc7a965fdc942e21b6f3d63c003e29ffe
hessamasna <hessamasna@yahoo.com>
2021-08-22 11:38:06 +0430

    login service

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef , ViewChild } from '@angular/core';
+import {LoginService} from "./login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('username') emailInput!: ElementRef;
+  @ViewChild('password') passwordInput!: ElementRef;
 
-  constructor() { }
+  constructor(private  loginServer:LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public async loginClick(){
+    const email = this.emailInput.nativeElement.value;
+    const password = this.passwordInput.nativeElement.value;
+
+    try {
+      const token = await this.loginServer.login(email, password);
+      await this.router.navigateByUrl('/dashboard');
+    } catch {
+      console.log('ERROR');
+    }
+  }
 }
